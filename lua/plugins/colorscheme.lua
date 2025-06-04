f6d998a712c1c4a7469800673bedbd0bbb65f230
Mitch <MitchoppDEV@gmail.com>
2025-06-04 19:54:12 +0100

    modulised my config and did some small changes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,9 +1,13 @@
+local function enable_transparency()
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+end
 return {
     {
         'rebelot/kanagawa.nvim',
         priority = 1000,
         config = function()
             vim.cmd("colorscheme kanagawa-dragon")
+            enable_transparency()
         end
     }
 }
