a3885221ba93ac456a55c97dcbd9020ed01e76e4
Mitch <MitchoppDEV@gmail.com>
2025-06-09 23:16:36 +0100

    added hardtime.nvim, some animation plugin and removed the transparency inside colorscheme.lua

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,13 +1,14 @@
-local function enable_transparency()
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-end
+-- local function enable_transparency()
+--     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+-- end
+
 return {
     {
         'rebelot/kanagawa.nvim',
         priority = 1000,
         config = function()
             vim.cmd("colorscheme kanagawa-dragon")
-            enable_transparency()
+            -- enable_transparency()
         end
     }
 }
---
+++ lua/plugins/hardtime.lua
@@ -0,0 +1,8 @@
+-- I need to get better at neovim so im going to force myself to use this for a while
+-- Atleast until it annoys me too much or i get good enough it dosen't bother me.
+return {
+  'm4xshen/hardtime.nvim',
+  lazy = false,
+  dependencies = { 'MunifTanjim/nui.nvim' },
+  opts = {},
+}
