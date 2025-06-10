9a6b158f760fcaee6da367f3fa5471c9572b032a
Mitch <MitchoppDEV@gmail.com>
2025-06-10 22:39:24 +0100

    added a new lsp config i actually understand, removed the rust lsp since that is handeled seperately. Also updated the lsp to work with vim properly and show things correctly

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -14,3 +14,12 @@ vim.opt.cursorline = true
 vim.opt.undofile = true
 
 print("options are working.")
+
+vim.diagnostic.config({
+  virtual_text = true,
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+  severity_sort = true,
+})
+
