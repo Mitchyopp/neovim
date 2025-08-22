5dccb6ddd3372cf854e639bc27c71c365e1c6276
Mitchy <MitchoppDEV@gmail.com>
2025-08-22 02:04:36 +0100

    cleaned up and fixed the install

--- README.md
+++ README.md
@@ -8,14 +8,17 @@ This is a basic neovim config using pack.nvim with everything inside `init.lua`
 > If git clone fails or the plugins don't install then make sure you have SSH setup.
 
 > [!IMPORTANT]
-> Make sure you don't have a config located already at /home/YourUser/.config/nvim
+> Make sure you are using neovim version 12.0+ 
+
+> [!TIP]
+> Make sure you don't have a config located already at /home/YourUser/.config/nvim (The install script does this automatically)
 > If you do you can make it a backup with:
 ```mv $HOME/.config/nvim $HOME/.config/nvim_backup```
 
 ## Install
 Install script (Always read a script before executing it to make sure it's safe!)
 
-```curl -fsSL https://github.com/Mitchyopp/neovim/install.sh | bash```
+```curl -fsSL https://raw.githubusercontent.com/Mitchyopp/neovim/main/install.sh | bash```
 
 Manual clone
 
--- install.sh
+++ install.sh
@@ -1,14 +1,22 @@
 #!/usr/bin/env bash
-set -euo pipefall
+set -euo pipefail
 
-if [ -d "$HOME/.config/nvim" ]; then
-	echo "Old config found"
+if ! command -v git >/dev/null 2>&1; then
+	echo "Error: git is not installed, please install git and try again." >&2
+	exit 1
+fi
+
+DIR="$HOME/.config/nvim"
+TS="$(date +%s)"
+
+if [ -d "$NVIM_DIR" ]; then
+	echo "Old config found at $NVIM_DIR"
 	echo "Backing up old config"
-	mv "$HOME/.config/nvim" "$HOME/.config/nvim.bak.$(date +%s)"
-	echo "New config is at $HOME/.config/nvim.bak.date..."
+	mv "$NVIM_DIR" "$NVIM_DIR.bak.$TS"
+	echo "Backup is located at: $NVIM_DIR.bak.$TS"
 fi
 
 echo "-------------------------------"
-echo "Cloning new config..."
-git clone https://github.com/Mitchyopp/neovim.git "$HOME/.config/nvim"
+echo "Cloning new config into $NVIM_DIR ..."
+git clone --depth 1 https://github.com/Mitchyopp/neovim.git "$NVIM_DIR"
 echo "Done! Thankyou for trying out my config :D"
